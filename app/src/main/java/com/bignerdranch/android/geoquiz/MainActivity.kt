ae10c55473378850da1f0986943e26eed6ae06fa
Nick <nicholas.claffey@outlook.com>
2020-01-04 19:52:14 -0600

    chapter3 challenge 2

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -3,10 +3,13 @@ package com.bignerdranch.android.geoquiz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import java.math.RoundingMode
+import kotlin.math.roundToLong
 
 private const val TAG = "MainActivity"
 
@@ -29,6 +32,11 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_asia, true, false)
 
     )
+
+    /**
+     * Chapter 3 challenge 2
+     */
+    private var correctAnswers = 0
 
 
     private var currentIndex = 0
@@ -96,17 +104,36 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkAnswer(userAnswer: Boolean){
+
+
         val correctAnswer = questionBank[currentIndex].answer
+
+        if(userAnswer == correctAnswer){
+            correctAnswers++
+        }
 
         val messageResId = if (userAnswer == correctAnswer){
             R.string.correct_toast
+
         }else{
             R.string.incorrect_toast
         }
         // update answer submitted to true
         questionBank[currentIndex].answerSubmitted = true
         setButtonStatus()
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+
+        /**
+         * Chapter 3 challenge 2
+         */
+        if(isQuizComplete()){
+            var score = (correctAnswers * 100.0) / questionBank.size
+            var rounded = score.toBigDecimal().setScale(2,RoundingMode.UP).toDouble()
+            var message = resources.getString(messageResId) + " Quiz complete: $rounded %"
+            Toast.makeText(this, message ,Toast.LENGTH_SHORT).show()
+        }else {
+            Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     /**
@@ -120,5 +147,30 @@ class MainActivity : AppCompatActivity() {
             trueButton.isEnabled = true
             falseButton.isEnabled = true
         }
+    }
+
+    /**
+     * Chapter 3 challenge 2
+     */
+    private fun isQuizComplete(): Boolean{
+        for(question in questionBank){
+            if(!question.answerSubmitted){
+                return false
+            }
+        }
+        return true
+    }
+
+    /**
+     * Chapter 3 challenge 2
+     */
+    private fun displayScore(){
+        var score = (correctAnswers * 100.0) / questionBank.size
+        //This needs to be a resource ID
+        var message = " quiz complete! Score: $score"
+
+        var toast = Toast.makeText(this, message, Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.TOP, Gravity.CENTER, 200)
+        toast.show()
     }
 }
