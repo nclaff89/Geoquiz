255e9f2f1b99c2c2fe44db4f08ed232cac5acaed
Nick <nicholas.claffey@outlook.com>
2020-01-10 17:37:30 -0600

    completed chapter 7 challenge 2, added code from chapter 6 challenge 1 to fix issue with screen rotation in cheat activity

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 
@@ -12,6 +13,14 @@ const val EXTRA_ANSWER_SHOWN = "com.bignerdranch.android.geoquiz.answer_shown"
 
 private const val EXTRA_ANSWER_IS_TRUE =
     "com.bignerdranch.android.geoquiz.answer_is_true"
+
+/**
+ * Reusing chapter 6 challenge 1 code for
+ * chapter 7 cahllenge 2
+ */
+private const val KEY_CHEATER = "cheater" // create a key for cheater status for saveInstaneState
+
+private const val TAG = "CheatActivity" //TAG for log cat debugging
 
 
 class CheatActivity : AppCompatActivity() {
@@ -21,21 +30,64 @@ class CheatActivity : AppCompatActivity() {
 
     private var answerIsTrue = false
 
+    /**
+     * Challenge
+     */
+    private var cheaterStatus = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cheat)
 
+        /**
+         * Chapter 6 challenge 1
+         */
+        //get savedInstnaceState and set cheater status accordingly
+        cheaterStatus = savedInstanceState?.getBoolean(KEY_CHEATER, false) ?: false
+
         answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
         answerTextView = findViewById(R.id.answer_text_view)
+
         showAnswerButton = findViewById(R.id.show_answer_button)
-        showAnswerButton.setOnClickListener {
-            val answerText = when{
-                answerIsTrue -> R.string.true_button
-                else -> R.string.false_button
-            }
-            answerTextView.setText(answerText)
-            setAnswerShownResult(true)
+
+        /**
+         * Chapter 6 challeneg 1 / chap 7 challenge 2
+         */
+        if(cheaterStatus){
+            cheatButtonClicked()
         }
+
+
+        //eliminate duplicate code by creating function to handle it
+        showAnswerButton.setOnClickListener {
+            cheatButtonClicked()
+
+        }
+    }
+
+    /**
+     * Chapter 6 challenge 1 / chap 7 challenge 2
+     */
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        Log.i(TAG,"onSaveInstanceState")
+        savedInstanceState.putBoolean(KEY_CHEATER, cheaterStatus)
+    }
+
+    /**
+     * Chapter 6 challenge 1 // chapter 7 challenge 2
+     * function to just eliminate some duplicate code
+     */
+    private fun cheatButtonClicked(){
+        val answerText = when{
+            answerIsTrue -> R.string.true_button
+            else -> R.string.false_button
+        }
+        answerTextView.setText(answerText)
+        setAnswerShownResult(true)
+
+        //just a quick way to track whether user cheated or not
+        cheaterStatus = true
     }
 
     private fun setAnswerShownResult(isAnswerShown: Boolean){
